8aabdb47f8ae62b8219485baf01b6437a17496ed
Ankit Kumar <ankitkumar95993@gmail.com>
2023-11-29 16:41:42 +0530

    Connection to database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
-const express  = require('express')
+const express  = require('express');
+const { dbconnect } = require('./config/database');
+
 
 const app = express();
+
+dbconnect();
 
 app.listen(3000,()=>{
     console.log('server is running at port no 3000!');
