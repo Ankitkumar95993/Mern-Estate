03731e03bfd5422fea04f1bc61d96d3092d8ca00
Ankit Kumar <ankitkumar95993@gmail.com>
2023-12-03 08:42:43 +0530

    create signin api route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,12 +1,13 @@
 const User = require("../models/user.model");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const { errorHandler } = require("../utils/error");
 
-exports.signup = async (req, res,next) => {
+exports.signup = async (req, res, next) => {
   try {
     const { username, email, password } = req.body;
     if (!username || !email || !password) {
-      return res.status(403).send({
+      return res.status(403).json({
         success: false,
         message: "all fields are required",
       });
@@ -18,6 +19,35 @@ exports.signup = async (req, res,next) => {
       success: true,
       message: "User registered successfully",
     });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(403).json({
+      success: false,
+      message: "please fill up all the fields",
+    });
+  }
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Wrong Credential"));
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const {password:pass,...rest} = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
