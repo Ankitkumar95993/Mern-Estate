26566549eaa031eeaa165042a460557f7f4fe5c7
Ankit Kumar <ankitkumar95993@gmail.com>
2023-12-24 12:21:48 +0530

    resolving rendering issue

--- backend/index.js
+++ backend/index.js
@@ -30,10 +30,11 @@ app.use('/api/v1/user',userRouter);
 app.use('/api/v1/auth',authRouter);
 app.use('/api/v1/listing',listingRouter);
 
-app.use(express.static(path.join(__dirname,'/client/dist')));
+app.use(express.static(path.resolve(__dirname,'client','dist')));
 
 app.get('*',(req,res)=>{res.sendFile(path.resolve(__dirname,'client','dist','index.html'))
 })
+
 
 
 app.use((err,req,res,next)=>{
